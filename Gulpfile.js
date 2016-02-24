a816790c632d26a85cc63559341d26c3c822ac28
Brandon Newton <bitwiselove@gmail.com>
2016-02-24 13:40:12 -0800

    minify css, add google analytics

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,12 +3,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', () => {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('main.css'))
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('build', () => {
+  gulp.src('css/**/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', () => {
